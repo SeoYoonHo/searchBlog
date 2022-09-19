aafe6a9d68aaa1a9e61816de44f3605497c04f46
seoyoonho <gogoy2643@naver.com>
2022-09-19 23:40:18 +0900

    불필요한 import 제거

--- src/main/java/com/api/searchblog/service/BlogApiService.java
+++ src/main/java/com/api/searchblog/service/BlogApiService.java
@@ -7,12 +7,9 @@ import com.api.searchblog.dto.PopularKeywordResponseDTO;
 import com.api.searchblog.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
